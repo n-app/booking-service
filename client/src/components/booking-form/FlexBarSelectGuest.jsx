2271a79c0f595f7ff703c34b0869b5f881a33b3a
Logan Hart <2loganhart@gmail.com>
2018-06-11 17:21:34 -0700

    Ignored bundle main.js

--- client/src/components/booking-form/FlexBarSelectGuest.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const FlexBarSelectGuest = () => (
-  <div className="guest-count-filter-flexbar-container">
-    test
-  </div>
-);
-
-export default FlexBarSelectGuest;
---
+++ client/src/components/booking-form/select-guest-flexbar/FlexBarSelectGuestButtons.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const FlexBarSelectGuestButtons = (props) => (
+  <div className="flexbar-content-icon">
+    <div className="filter-flexbar-buttons-container">
+      <div className="filter-flexbar-button-left-container">
+        left
+      </div>
+      <div className="filter-flexbar-button-value-container">
+        1
+      </div>
+      <div className="filter-flexbar-button-right-container">
+        right
+      </div>
+    </div>
+  </div>
+);
+
+export default FlexBarSelectGuestButtons;
